687a87b0b67320048d5f58bbed911fe24d683b2e
Brian Torres Alvarado <briantorres@Brians-MacBook-Pro.local>
2025-03-28 19:00:27 -0400

    Black background

--- game_gl.cpp
+++ game_gl.cpp
@@ -125,7 +125,7 @@ void GameGL::Update()
 void GameGL::Render()
 {
     //Background color
-    glClearColor(0.7f, 0.13f, 0.17f, 1.0f);
+    glClearColor(0, 0, 0, 0);
     //Clean the back buffer and assing the new color
     glClear(GL_COLOR_BUFFER_BIT);
 
